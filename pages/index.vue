ac4d9d847274044d45cdb3a1a4a98b4d8345e9ed
pnrnc485 <kenta.suzuki1014@gmail.com>
2019-11-08 21:00:48 +0900

    pages/index追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,17 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        inucolo
-      </h1>
-      <h2 class="subtitle">
-        My transcendent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <nuxt-link to="/breeds" class="navbar-item">イヌコロ</nuxt-link>
+          <span class="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
       </div>
-    </div>
+    </nav>
   </div>
 </template>
 
